1848d51e1728c6f67ad12da7968943ffce9727c7
Subhansh Naithani <subhanshnaithani@example.com>
2025-02-08 01:17:29 +0530

    logout done

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -2,6 +2,7 @@ const { model } = require('mongoose');
 const userModel = require('../models/user.model');
 const userService = require('../services/user.service');
 const { validationResult } = require('express-validator');
+const blacklistTokenModel = require('../models/blacklistToken.model');
 
 module.exports.registerUser = async (req, res) => {
     const errors = validationResult(req);
@@ -35,8 +36,15 @@ module.exports.loginUser = async (req, res) => {
         return res.status(401).json({message: 'Invalid email or password'});
     }
     const token = user.generateAuthToken();
+    res.cookie('token', token);
     res.status(200).json({token,user});
 }
 module.exports.getUserProfile = async (req, res) => {
     res.status(200).json(req.user);
+}
+module.exports.logoutUser = async (req, res) => {
+    res.clearCookie('token');
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+    await blacklistTokenModel.create({token});
+    res.status(200).json({message: 'Logged out successfully'});
 }
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -19,4 +19,6 @@ router.post('/login', [
 
 router.get('/profile',authMiddleware.authUser, userController.getUserProfile)
 
+router.get('/logout', userController.logoutUser)
+
 module.exports = router;
